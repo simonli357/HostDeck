80c82f03e47d73d0c3dfabe4b8ac22e5fee15bae
Simon Li <xi.yang.li@mail.mcgill.ca>
2026-07-28 06:46:22 -0400

    [fix] Keep accessibility fixtures internal

--- packages/test-fixtures/src/index.ts
+++ packages/test-fixtures/src/index.ts
@@ -1,5 +1,3 @@
-export * from "./accessibility-matrix.js";
 export * from "./mobile-design-contract.js";
 export * from "./remote-ingress.js";
-export * from "./responsive-layout-matrix.js";
 export * from "./structured-runtime.js";
